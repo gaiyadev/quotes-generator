0f7d79a3b0b884544da46744df03e6bcc3db862a
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-07-02 17:40:16 +0100

    Feat: done with the app. now fetehing qoutes and displayin

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import QuoteAndAuthor from './components/Quotes/Quotes';
+import Quotes from './database/QuotesDb';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      quoteData: {
+        quote: Quotes[0].quote,
+        author: Quotes[0].author,
+      }
+
+    };
+  }
+
+  randomQuote = () => {
+    const randomNumber = Math.floor(Math.random() * Quotes.length);
+    // console.log(Quotes.length);
+    return Quotes[randomNumber];
+  }
+
+  shuffleQuotes = (array) => {
+    return array.sort(() => Math.random() - 1);
+  }
+
+  handleClick = () => {
+    const generateRandomQuote = this.randomQuote();
+    this.setState({
+      quote: generateRandomQuote.quote,
+      author: generateRandomQuote.author
+    });
+    this.shuffleQuotes(Quotes)
+  }
+
+  componentDidMount() {
+
+  }
+
+  randomColor = () => {
+    const color = `rgb(
+      ${Math.floor(Math.random() * 25)},
+      ${Math.floor(Math.random() * 155)},
+      ${Math.floor(Math.random() * 55)})`;
+    return color;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <QuoteAndAuthor
+          displayColor={this.randomColor}
+          handleClick={this.handleClick}
+          {...this.state.quoteData}
+        />
+      </div>
+    );
+  }
 }
-
 export default App;
